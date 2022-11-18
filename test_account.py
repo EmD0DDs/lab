d0328dc9765307ef31351ee5658115a4db8b819f
EmD0DDs <118493545+EmD0DDs@users.noreply.github.com>
2022-11-18 15:13:32 -0600

    update test_account.py
    checked balance after every change to account balance

--- test_account.py
+++ test_account.py
@@ -18,26 +18,50 @@ class Test:
 
     def test_deposit(self):
         assert self.p1.deposit(500) is True
+        assert self.p1.get_balance() == 500
+
         assert self.p1.deposit(0) is False
+        assert self.p1.get_balance() == 500
+
         assert self.p1.deposit(-1) is False
         assert self.p1.get_balance() == 500
 
         assert self.p2.deposit(300) is True
+        assert self.p2.get_balance() == 300
+
         assert self.p2.deposit(0) is False
+        assert self.p2.get_balance() == 300
+
         assert self.p2.deposit(-1) is False
         assert self.p2.get_balance() == 300
 
     def test_withdraw(self):
         self.p1.deposit(200)
+        assert self.p1.get_balance() == 200
+
         assert self.p1.withdraw(100) is True
+        assert self.p1.get_balance() == 100
+
         assert self.p1.withdraw(0) is False
+        assert self.p1.get_balance() == 100
+
         assert self.p1.withdraw(-1) is False
+        assert self.p1.get_balance() == 100
+
         assert self.p1.withdraw(600) is False
         assert self.p1.get_balance() == 100
 
         self.p2.deposit(200)
+        assert self.p2.get_balance() == 200
+
         assert self.p2.withdraw(100) is True
+        assert self.p2.get_balance() == 100
+
         assert self.p2.withdraw(0) is False
+        assert self.p2.get_balance() == 100
+
         assert self.p2.withdraw(-1) is False
+        assert self.p2.get_balance() == 100
+
         assert self.p2.withdraw(400) is False
         assert self.p2.get_balance() == 100
