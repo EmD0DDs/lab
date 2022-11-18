8efcc2ad5c6624e679fafd6c802653d3f7ef6b5c
EmD0DDs <emmagdodds@gmail.com>
2022-11-18 13:00:48 -0600

    Update account.py
    account with docstrings

--- account.py
+++ account.py
@@ -1,9 +1,19 @@
 class Account:
     def __init__(self, name):
+        """
+        function to establish account object
+        :param name: first name of person
+        account_balance automatically set to 0
+        """
         self.__account_name = name
         self.__account_balance = 0
 
     def deposit(self, amount):
+        """
+        function to increase account_balance based on input
+        :param amount: amount of $ deposited into account
+        :return: True if transaction affected account, False if not
+        """
         if amount > 0:
             self.__account_balance += amount
             return True
@@ -11,6 +21,11 @@ class Account:
             return False
 
     def withdraw(self, amount):
+        """
+        function to decrease account_balance based on input
+        :param amount: amount of money to take out of account (must be more than $0 and less than current balance)
+        :return: True if transaction affected account and false if not
+        """
         if amount > 0 and amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
@@ -18,8 +33,16 @@ class Account:
             return False
 
     def get_balance(self):
+        """
+        function to return account balance
+        :return: current account balance
+        """
         return self.__account_balance
 
     def get_name(self):
+        """
+        function to return account name
+        :return: returns name account in under
+        """
         return self.__account_name
 
