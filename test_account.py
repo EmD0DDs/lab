da6ac3b52abea61bbf871cc1545642d55a8d6751
EmD0DDs <118493545+EmD0DDs@users.noreply.github.com>
2022-11-20 17:12:39 -0600

    Update test_account.py

--- test_account.py
+++ test_account.py
@@ -1,4 +1,5 @@
 from account import *
+from pytest import *
 
 
 class Test:
@@ -17,51 +18,51 @@ class Test:
         assert self.p2.get_balance() == 0
 
     def test_deposit(self):
-        assert self.p1.deposit(500) is True
-        assert self.p1.get_balance() == 500
+        assert self.p1.deposit(20.5) is True
+        assert self.p1.get_balance() == approx(20.5, abs=0.001)
 
         assert self.p1.deposit(0) is False
-        assert self.p1.get_balance() == 500
+        assert self.p1.get_balance() == approx(20.5, abs=0.001)
 
         assert self.p1.deposit(-1) is False
-        assert self.p1.get_balance() == 500
+        assert self.p1.get_balance() == approx(20.5, abs=0.001)
 
-        assert self.p2.deposit(300) is True
-        assert self.p2.get_balance() == 300
+        assert self.p2.deposit(50.75) is True
+        assert self.p2.get_balance() == approx(50.75, abs=0.001)
 
         assert self.p2.deposit(0) is False
-        assert self.p2.get_balance() == 300
+        assert self.p2.get_balance() == approx(50.75, abs=0.001)
 
         assert self.p2.deposit(-1) is False
-        assert self.p2.get_balance() == 300
+        assert self.p2.get_balance() == approx(50.75, abs=0.001)
 
     def test_withdraw(self):
         self.p1.deposit(200)
         assert self.p1.get_balance() == 200
 
-        assert self.p1.withdraw(100) is True
-        assert self.p1.get_balance() == 100
+        assert self.p1.withdraw(50.50) is True
+        assert self.p1.get_balance() == approx(149.5, abs=0.001)
 
         assert self.p1.withdraw(0) is False
-        assert self.p1.get_balance() == 100
+        assert self.p1.get_balance() == approx(149.5, abs=0.001)
 
         assert self.p1.withdraw(-1) is False
-        assert self.p1.get_balance() == 100
+        assert self.p1.get_balance() == approx(149.5, abs=0.001)
 
         assert self.p1.withdraw(600) is False
-        assert self.p1.get_balance() == 100
+        assert self.p1.get_balance() == approx(149.5, abs=0.001)
 
         self.p2.deposit(200)
         assert self.p2.get_balance() == 200
 
-        assert self.p2.withdraw(100) is True
-        assert self.p2.get_balance() == 100
+        assert self.p2.withdraw(50.5) is True
+        assert self.p2.get_balance() == approx(149.5, abs=0.001)
 
         assert self.p2.withdraw(0) is False
-        assert self.p2.get_balance() == 100
+        assert self.p2.get_balance() == approx(149.5, abs=0.001)
 
         assert self.p2.withdraw(-1) is False
-        assert self.p2.get_balance() == 100
+        assert self.p2.get_balance() == approx(149.5, abs=0.001)
 
         assert self.p2.withdraw(400) is False
-        assert self.p2.get_balance() == 100
+        assert self.p2.get_balance() == approx(149.5, abs=0.001)
